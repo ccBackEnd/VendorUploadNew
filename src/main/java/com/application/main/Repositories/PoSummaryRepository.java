880838301da7b14b8fca7bef07257eee0a9efe1c
PRANAV <PRANAV@PRANAV-CC.costacloud.in>
2024-07-26 18:10:26 +0530

    new

---
+++ src/main/java/com/application/main/Repositories/PoSummaryRepository.java
@@ -0,0 +1,16 @@
+package com.application.main.Repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.application.main.model.PoSummary;
+
+
+public interface PoSummaryRepository extends MongoRepository<PoSummary, String> {
+
+	Optional<PoSummary> findByPoNumber(String poNumber);
+	List<PoSummary> findByEic(String eic);
+	List<PoSummary> findByPoStatus(String poStatus);
+}
